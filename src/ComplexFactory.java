2a74d11b15c02637545f80f3d5877acfd0f78884
«dima» <dima-titov-93@mail.ru>
2023-09-22 19:04:16 +0300

    Initial commit

---
+++ src/ComplexFactory.java
@@ -0,0 +1,9 @@
+abstract class ComplexFactory {
+    abstract Complex add(Complex other);
+
+    abstract Complex subtract(Complex other);
+
+    abstract Complex multiply(Complex other);
+
+    abstract Complex divide(Complex other);
+}
